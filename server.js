c073e21d4c880b39228d5aec28a5edaa48a6dca5
faelzx7 <rafa.alencar.silva2006@gmail.com>
2026-03-25 14:56:08 -0300

    fix: allow Google GSI domains in CSP for obrigado page

--- server.js
+++ server.js
@@ -137,12 +137,12 @@ app.use((req, res, next) => {
   // TODO: migrar para nonce-based CSP para eliminar 'unsafe-inline'
   res.setHeader('Content-Security-Policy',
     "default-src 'self'; " +
-    "script-src 'self' 'unsafe-inline' https://accounts.google.com; " +
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "script-src 'self' 'unsafe-inline' https://accounts.google.com https://apis.google.com; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com; " +
     "font-src 'self' https://fonts.gstatic.com; " +
     "img-src 'self' data: https:; " +
-    "connect-src 'self' https://accounts.google.com; " +
-    "frame-src https://accounts.google.com;"
+    "connect-src 'self' https://accounts.google.com https://play.google.com; " +
+    "frame-src https://accounts.google.com https://apis.google.com;"
   );
   next();
 });
